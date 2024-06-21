776fad7928dc24a780d4778e7bf51adb02cdaa37
Henrik Böving <hargonix@gmail.com>
2024-06-21 16:27:12 +0200

    feat: Cpa implementation

---
+++ Cpa/Cpa.lean
@@ -0,0 +1,71 @@
+import Cpa.Cfa
+import Cpa.Domains.Def
+import Cpa.Domains.Combination
+import Cpa.Domains.Location
+import Lean.Data.HashMap
+
+namespace Cpa
+
+open Lean
+open Domain
+
+structure Config (d : Type) where
+  merge : d → d → d
+  stop : d → Array d → Bool
+  cfa : CFA
+
+variable {d : Type} [LE d] [DecidableRel (@LE.le d _)] [Domain d] [BEq d] [Transfer d]
+
+partial def run (config : Config (Combined Location d)) (e0 : d) : Array (Combined Location d) :=
+  let init := ⟨.loc 0, e0⟩
+  go [init] #[init]
+where
+  go (waitlist : List (Combined Location d)) (reached : Array (Combined Location d))
+      : Array (Combined Location d) := Id.run do
+    match waitlist with
+    | [] => reached
+    | ⟨l, e⟩ :: waitlist =>
+      match l with
+      | .top => go waitlist reached
+      | .loc idx =>
+        let mut waitlist := waitlist
+        let mut reached := reached
+        let edges := config.cfa.getEdges idx
+        for edge in edges do
+          let e' := Transfer.transfer ⟨l, e⟩ idx edge
+          for e'' in reached do
+            let e_new := config.merge e' e''
+            if e_new != e'' then
+              waitlist := e_new :: waitlist
+              reached := reached.push e_new
+          if !config.stop e' reached then
+              waitlist := e' :: waitlist
+              reached := reached.push e'
+        go waitlist reached
+
+def mcMerge (_lhs rhs : Combined Location d) : Combined Location d := rhs
+def dfaMerge (lhs rhs : Combined Location d) : Combined Location d :=
+  if lhs.left == rhs.left then
+    Domain.meet lhs rhs
+  else
+    rhs
+
+def defaultStop (state : Combined Location d) (reached : Array (Combined Location d)) : Bool :=
+  let ⟨l, e⟩ := state
+  reached.any (fun ⟨l', e'⟩ => l' = l ∧ e ≤ e')
+
+def Config.modelChecking (d : Type) [LE d] [DecidableRel (@LE.le d _)] (cfa : CFA) : Config (Combined Location d) :=
+  {
+    merge := mcMerge,
+    stop := defaultStop,
+    cfa := cfa
+  }
+
+def Config.dataFlowAnalysis (d : Type) [LE d] [DecidableRel (@LE.le d _)] [Domain d] (cfa : CFA) : Config (Combined Location d) :=
+  {
+    merge := dfaMerge,
+    stop := defaultStop,
+    cfa := cfa
+  }
+
+end Cpa
